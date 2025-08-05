07af78436b60096553064aa04a276793abc97aff
mko1989 <44507953+mko1989@users.noreply.github.com>
2025-08-05 09:43:59 +0200

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export function setupWebServer(instance) {
 	const app = express()
